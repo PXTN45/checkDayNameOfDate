18a3b9c372e4ada763ace28fa8133c045cb8fcc1
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-04-10 14:07:58 +0700

    checkDayNameOfDate

---
+++ src/checkDayNameOfDate/TC002checkDay.java
@@ -0,0 +1,36 @@
+package checkDayNameOfDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class TC002checkDay {
+
+	@Test
+	void test01() throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//Home
+		driver.get("http://localhost:3000/index.php");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("Nattaphong Sriphaophan");
+//		driver.findElement(By.xpath("//*[@id=\"day\"]")).sendKeys("2023/04/10");
+		driver.findElement(By.xpath("//*[@id=\"score\"]")).sendKeys("99");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//*[@id=\"btn\"]")).click();
+		Thread.sleep(4000);
+		String result = driver.switchTo().alert().getText();
+		assertEquals("Please Enter Day", result);
+		driver.switchTo().alert().accept();
+		Thread.sleep(4000);
+		driver.close();
+		
+	}
+
+}
